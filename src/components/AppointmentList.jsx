d6ee36da590ec1ec61cc333b83de9dcc5b4d03a8
Nitesh <nitesh@mail.prismetric.com>
2025-12-19 13:12:48 +0530

    style(appointment): remove unnecessary blank lines
    Cleans up formatting in AppointmentList to improve readability
    and maintain a consistent code structure.

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -203,6 +203,8 @@ const AppointmentList = ({ appointments, onDeleteAppointment }) => {
                 </div>
             </div>
 
+
+
             {/* Detail Modal */}
             {selectedAppointment && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
